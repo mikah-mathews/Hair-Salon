1dae595066fc766b306eff83f25553aa19dc3e05
Mikah Mathews <mikah@outofband.org>
2022-03-11 15:25:09 -0800

    WIP: Create stylist form

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -10,15 +10,28 @@ namespace HairSalon.Controllers
   public class StylistsController : Controller
   {
     private readonly HairSalonContext _db;
-    public HairSalonController(HairSalonContext db)
+    public StylistsController(HairSalonContext db)
     {
       _db = db;
     }
 
     public ActionResult Index()
     {
-      List<Stylist> model = _db.Stylists.Include(stylist => stylist.Client).ToList();
+      List<Stylist> model = _db.Stylists.ToList();
       return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Stylist stylist)
+    {
+      _db.Stylists.Add(stylist);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
     }
   }
 }
--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -3,16 +3,11 @@ namespace HairSalon.Models
 {
   public class Client
   {
-    public Client()
-    {
-      this.Stylists = new HashSet<Stylist>();
-    }
-
     public int ClientId { get; set; }
     public string ClientName { get; set; }
     public int PhoneNumber { get; set; }
     public string AppointmentDate { get; set; }
     public string Service { get; set; }
-    public virtual ICollection<Stylist> Stylists { get; set; }
+    public virtual Stylist Stylist { get; set; }
   }
 }
