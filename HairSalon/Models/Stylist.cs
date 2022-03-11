002c267f7f299eb272e97c657dde5005cc346dd7
Mikah Mathews <mikah@outofband.org>
2022-03-11 11:53:26 -0800

    update readme, add start of model logic

---
+++ HairSalon/Models/Stylist.cs
@@ -0,0 +1,9 @@
+namespace HairSalon.Models
+{
+  public class Stylist
+  {
+    public int StylistId { get; set; }
+    public string StylistName { get; set; }
+    public virtual Client Client { get; set; }
+  }
+}
