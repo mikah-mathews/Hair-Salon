33a92f275d37bd0027e664cf9327cf16ea17fb64
Mikah Mathews <mikah@outofband.org>
2022-03-21 13:10:41 -0700

    Update create and controller

---
+++ HairSalon/Controllers/ClientsController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HairSalon.Controllers
+{
+  public class ClientsController : Controller
+  {
+    
+  }
+}
